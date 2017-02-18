b394d43f176b49e62ef6b6bcabae9847d430888b
viktoria <leonieneon@gmail.com>
2017-02-18 16:36:13 +0100

    add active bluetooth device model

---
+++ app/src/main/java/com/lady/viktoria/lightdrip/DatabaseModels/ActiveBluetoothDevice.java
@@ -0,0 +1,31 @@
+package com.lady.viktoria.lightdrip.DatabaseModels;
+
+import io.realm.RealmObject;
+
+public class ActiveBluetoothDevice extends RealmObject {
+
+    private String name;
+    public String getname() {
+        return name;
+    }
+    public void setname(final String name) {
+        this.name = name;
+    }
+
+    private String address;
+    public String getaddress() {
+        return address;
+    }
+    public void setaddress(final String address) {
+        this.address = address;
+    }
+
+    private boolean connected;
+    public Boolean getconnected() {
+        return connected;
+    }
+    public void setconnected(final boolean connected) {
+        this.connected = connected;
+    }
+
+}
