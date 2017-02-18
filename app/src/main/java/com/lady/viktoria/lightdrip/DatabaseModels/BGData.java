aa6d8709046c81d0621a9a2539731c2effbb48db
viktoria <leonieneon@gmail.com>
2017-02-18 15:58:28 +0100

    reformat set & getter

--- app/src/main/java/com/lady/viktoria/lightdrip/DatabaseModels/BGData.java
+++ app/src/main/java/com/lady/viktoria/lightdrip/DatabaseModels/BGData.java
@@ -10,291 +10,170 @@ public class BGData extends RealmObject {
     @SerializedName("a")
     @Expose
     private Double a;
-    @SerializedName("age_adjusted_raw_value")
-    @Expose
-    private Double ageAdjustedRawValue;
-    @SerializedName("b")
-    @Expose
-    private Double b;
-    @SerializedName("c")
-    @Expose
-    private Double c;
-    @SerializedName("calculated_value")
-    @Expose
-    private Double calculatedValue;
-    @SerializedName("calculated_value_slope")
-    @Expose
-    private Double calculatedValueSlope;
-    @SerializedName("calibration_flag")
-    @Expose
-    private Boolean calibrationFlag;
-    @SerializedName("calibration_uuid")
-    @Expose
-    private String calibrationUuid;
-    @SerializedName("filtered_data")
-    @Expose
-    private Double filteredData;
-    @SerializedName("ra")
-    @Expose
-    private Double ra;
-    @SerializedName("raw_data")
-    @Expose
-    private Double rawData;
-    @SerializedName("rb")
-    @Expose
-    private Double rb;
-    @SerializedName("rc")
-    @Expose
-    private Double rc;
-    @SerializedName("sensor_uuid")
-    @Expose
-    private String sensorUuid;
-    @SerializedName("time_since_sensor_started")
-    @Expose
-    private Double timeSinceSensorStarted;
-    @SerializedName("timestamp")
-    @Expose
-    private double timestamp;
-    @SerializedName("uuid")
-    @Expose
-    private String uuid;
-
-    /**
-     * @return The a
-     */
-    public Double getA() {
+    public double geta() {
         return a;
     }
-
-    /**
-     * @param a The a
-     */
-    public void setA(Double a) {
+    public void seta(final double a) {
         this.a = a;
     }
 
-    /**
-     * @return The ageAdjustedRawValue
-     */
-    public Double getAgeAdjustedRawValue() {
+    @SerializedName("age_adjusted_raw_value")
+    @Expose
+    private Double ageAdjustedRawValue;
+    public double getageAdjustedRawValue() {
         return ageAdjustedRawValue;
     }
-
-    /**
-     * @param ageAdjustedRawValue The age_adjusted_raw_value
-     */
-    public void setAgeAdjustedRawValue(Double ageAdjustedRawValue) {
+    public void setageAdjustedRawValue(final double ageAdjustedRawValue) {
         this.ageAdjustedRawValue = ageAdjustedRawValue;
     }
 
-    /**
-     * @return The b
-     */
+    @SerializedName("b")
+    @Expose
+    private Double b;
     public Double getB() {
         return b;
     }
-
-    /**
-     * @param b The b
-     */
     public void setB(Double b) {
         this.b = b;
     }
 
-    /**
-     * @return The c
-     */
+    @SerializedName("c")
+    @Expose
+    private Double c;
     public Double getC() {
         return c;
     }
-
-    /**
-     * @param c The c
-     */
     public void setC(Double c) {
         this.c = c;
     }
 
-    /**
-     * @return The calculatedValue
-     */
+    @SerializedName("calculated_value")
+    @Expose
+    private Double calculatedValue;
     public Double getCalculatedValue() {
         return calculatedValue;
     }
-
-    /**
-     * @param calculatedValue The calculated_value
-     */
     public void setCalculatedValue(Double calculatedValue) {
         this.calculatedValue = calculatedValue;
     }
 
-    /**
-     * @return The calculatedValueSlope
-     */
+    @SerializedName("calculated_value_slope")
+    @Expose
+    private Double calculatedValueSlope;
     public Double getCalculatedValueSlope() {
         return calculatedValueSlope;
     }
-
-    /**
-     * @param calculatedValueSlope The calculated_value_slope
-     */
     public void setCalculatedValueSlope(Double calculatedValueSlope) {
         this.calculatedValueSlope = calculatedValueSlope;
     }
 
-    /**
-     * @return The calibrationFlag
-     */
+    @SerializedName("calibration_flag")
+    @Expose
+    private Boolean calibrationFlag;
     public Boolean getCalibrationFlag() {
         return calibrationFlag;
     }
-
-    /**
-     * @param calibrationFlag The calibration_flag
-     */
     public void setCalibrationFlag(Boolean calibrationFlag) {
         this.calibrationFlag = calibrationFlag;
     }
 
-    /**
-     * @return The calibrationUuid
-     */
+    @SerializedName("calibration_uuid")
+    @Expose
+    private String calibrationUuid;
     public String getCalibrationUuid() {
         return calibrationUuid;
     }
-
-    /**
-     * @param calibrationUuid The calibration_uuid
-     */
     public void setCalibrationUuid(String calibrationUuid) {
         this.calibrationUuid = calibrationUuid;
     }
 
-    /**
-     * @return The filteredData
-     */
+    @SerializedName("filtered_data")
+    @Expose
+    private Double filteredData;
     public Double getFilteredData() {
         return filteredData;
     }
-
-    /**
-     * @param filteredData The filtered_data
-     */
     public void setFilteredData(Double filteredData) {
         this.filteredData = filteredData;
     }
 
-    /**
-     * @return The ra
-     */
+    @SerializedName("ra")
+    @Expose
+    private Double ra;
     public Double getRa() {
         return ra;
     }
-
-    /**
-     * @param ra The ra
-     */
     public void setRa(Double ra) {
         this.ra = ra;
     }
 
-    /**
-     * @return The rawData
-     */
+    @SerializedName("raw_data")
+    @Expose
+    private Double rawData;
     public Double getRawData() {
         return rawData;
     }
-
-    /**
-     * @param rawData The raw_data
-     */
     public void setRawData(Double rawData) {
         this.rawData = rawData;
     }
 
-    /**
-     * @return The rb
-     */
+    @SerializedName("rb")
+    @Expose
+    private Double rb;
     public Double getRb() {
         return rb;
     }
-
-    /**
-     * @param rb The rb
-     */
     public void setRb(Double rb) {
         this.rb = rb;
     }
 
-    /**
-     * @return The rc
-     */
+    @SerializedName("rc")
+    @Expose
+    private Double rc;
     public Double getRc() {
         return rc;
     }
-
-    /**
-     * @param rc The rc
-     */
     public void setRc(Double rc) {
         this.rc = rc;
     }
 
-    /**
-     * @return The sensorUuid
-     */
+    @SerializedName("sensor_uuid")
+    @Expose
+    private String sensorUuid;
     public String getSensorUuid() {
         return sensorUuid;
     }
-
-    /**
-     * @param sensorUuid The sensor_uuid
-     */
     public void setSensorUuid(String sensorUuid) {
         this.sensorUuid = sensorUuid;
     }
 
-    /**
-     * @return The timeSinceSensorStarted
-     */
+    @SerializedName("time_since_sensor_started")
+    @Expose
+    private Double timeSinceSensorStarted;
     public Double getTimeSinceSensorStarted() {
         return timeSinceSensorStarted;
     }
-
-    /**
-     * @param timeSinceSensorStarted The time_since_sensor_started
-     */
     public void setTimeSinceSensorStarted(Double timeSinceSensorStarted) {
         this.timeSinceSensorStarted = timeSinceSensorStarted;
     }
 
-    /**
-     * @return The timestamp
-     */
+    @SerializedName("timestamp")
+    @Expose
+    private double timestamp;
     public double getTimestamp() {
         return timestamp;
     }
-
-    /**
-     * @param timestamp The timestamp
-     */
     public void setTimestamp(double timestamp) {
         this.timestamp = timestamp;
     }
 
-    /**
-     * @return The uuid
-     */
+    @SerializedName("uuid")
+    @Expose
+    private String uuid;
     public String getUuid() {
         return uuid;
     }
-
-    /**
-     * @param uuid The uuid
-     */
     public void setUuid(String uuid) {
         this.uuid = uuid;
     }
-
 }
