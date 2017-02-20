356c15beda2b3d75f7785e963345a81d79d31238
viktoria <leonieneon@gmail.com>
2017-02-20 06:05:49 +0100

    add floating action button menu

--- app/src/main/java/com/lady/viktoria/lightdrip/MainActivity.java
+++ app/src/main/java/com/lady/viktoria/lightdrip/MainActivity.java
@@ -1,5 +1,6 @@
 package com.lady.viktoria.lightdrip;
 
+import android.animation.Animator;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
@@ -10,12 +11,14 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.lady.viktoria.lightdrip.DatabaseModels.ActiveBluetoothDevice;
@@ -29,9 +32,7 @@ public class MainActivity extends RealmBaseActivity implements View.OnClickListe
 
     private final static String TAG = MainActivity.class.getSimpleName();
 
-    Button browserrealm, addtimestamptodb;
     TextView bgmac;
-    Bundle b;
     public String mDeviceAddress = "00:00:00:00:00:00";
     public String mDeviceName;
     public String BTDeviceAddress = "00:00:00:00:00:00";
@@ -40,6 +41,10 @@ public class MainActivity extends RealmBaseActivity implements View.OnClickListe
     private boolean mConnected = false;
     private TextView mDataField;
     private Realm mRealm;
+    FloatingActionButton fab, fab1, fab2;
+    LinearLayout fabLayout1, fabLayout2;
+    View fabBGLayout;
+    boolean isFABOpen=false;
 
     // Code to manage Service lifecycle.
     private final ServiceConnection mServiceConnection = new ServiceConnection() {
@@ -70,14 +75,20 @@ public class MainActivity extends RealmBaseActivity implements View.OnClickListe
         setSupportActionBar(toolbar);
 
         bgmac = (TextView)findViewById(R.id.bgmac);
-        browserrealm = (Button) findViewById(R.id.browserealm);
-        browserrealm.setOnClickListener(this);
-        addtimestamptodb = (Button) findViewById(R.id.addtimestamp);
-        addtimestamptodb.setOnClickListener(this);
         mConnectionState = (TextView) findViewById(R.id.connection_state);
         Intent gattServiceIntent = new Intent(this, BGMeterGattService.class);
         bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
         mDataField = (TextView) findViewById(R.id.bgreading);
+        fabLayout1= (LinearLayout) findViewById(R.id.fabLayout1);
+        fabLayout2= (LinearLayout) findViewById(R.id.fabLayout2);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab1 = (FloatingActionButton) findViewById(R.id.fab1);
+        fab2= (FloatingActionButton) findViewById(R.id.fab2);
+        fabBGLayout=findViewById(R.id.fabBGLayout);
+        fab.setOnClickListener(this);
+        fab1.setOnClickListener(this);
+        fab2.setOnClickListener(this);
+        fabBGLayout.setOnClickListener(this);
         PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
 
         // Initialize Realm
@@ -166,24 +177,6 @@ public class MainActivity extends RealmBaseActivity implements View.OnClickListe
         return intentFilter;
     }
 
-    public void onClick(View v) {
-        // TODO Auto-generated method stub
-        switch (v.getId()) {
-            case R.id.browserealm:
-                RealmBrowser.startRealmModelsActivity(this, getRealmConfig());
-                break;
-            case R.id.addtimestamp:
-                mRealm = Realm.getInstance(getRealmConfig());
-                RealmResults<ActiveBluetoothDevice> results = mRealm.where(ActiveBluetoothDevice.class).findAll();
-                String address = results.last().getaddress();
-                Log.v(TAG, "address: " + address);
-                mRealm.close();
-                break;
-            default:
-                break;
-        }
-    }
-
     private void updateConnectionState(final int resourceId) {
         runOnUiThread(new Runnable() {
             @Override
@@ -243,6 +236,70 @@ public class MainActivity extends RealmBaseActivity implements View.OnClickListe
             Log.v(TAG, "Error try_set_realm_obj " + e.getMessage());
         } finally {
             mRealm.close();
+        }
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.fab:
+                if(!isFABOpen){
+                    showFABMenu();
+                }else{
+                    closeFABMenu();
+                }
+                break;
+            case R.id.fab1:
+                RealmBrowser.startRealmModelsActivity(this, getRealmConfig());
+                break;
+            case R.id.fab2:
+                mRealm = Realm.getInstance(getRealmConfig());
+                RealmResults<ActiveBluetoothDevice> results = mRealm.where(ActiveBluetoothDevice.class).findAll();
+                String address = results.last().getaddress();
+                mRealm.close();
+                final Snackbar snackBar = Snackbar.make(view, "get BT MAC From DB: " + address,
+                        Snackbar.LENGTH_INDEFINITE);
+                snackBar.setAction("Dismiss", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        snackBar.dismiss();
+                    }
+                }).show();
+                break;
+            case R.id.fabBGLayout:
+                closeFABMenu();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void showFABMenu(){
+        isFABOpen=true;
+        fabLayout1.setVisibility(View.VISIBLE);
+        fabLayout2.setVisibility(View.VISIBLE);
+        fabBGLayout.setVisibility(View.VISIBLE);
+
+        fab.animate().rotationBy(180);
+        fabLayout1.animate().translationY(-getResources().getDimension(R.dimen.standard_55));
+        fabLayout2.animate().translationY(-getResources().getDimension(R.dimen.standard_100));
+    }
+
+    private void closeFABMenu(){
+        isFABOpen=false;
+        fabBGLayout.setVisibility(View.GONE);
+        fab.animate().rotationBy(-180);
+        fabLayout1.animate().translationY(0);
+        fabLayout2.animate().translationY(0);
+        fabLayout1.setVisibility(View.GONE);
+        fabLayout2.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(isFABOpen){
+            closeFABMenu();
+        }else{
+            super.onBackPressed();
         }
     }
 }
