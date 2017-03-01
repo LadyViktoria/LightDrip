e156279a618b122aacd1911b1a3316cb404809e2
viktoria <leonieneon@gmail.com>
2017-03-01 16:26:17 +0100

    remove not needed libs

--- app/src/main/java/com/lady/viktoria/lightdrip/utils.java
+++ app/src/main/java/com/lady/viktoria/lightdrip/utils.java
@@ -1,13 +1,5 @@
 package com.lady.viktoria.lightdrip;
 
-
-import com.lady.viktoria.lightdrip.ImportedLibraries.dexcom.records.EGVRecord;
-import com.lady.viktoria.lightdrip.ImportedLibraries.dexcom.records.GlucoseDataSet;
-import com.lady.viktoria.lightdrip.ImportedLibraries.dexcom.records.SensorRecord;
-
-import java.util.Date;
-import java.util.TimeZone;
-
 public class utils {
 
     public static Integer convertSrc(final String Src) {
@@ -30,27 +22,5 @@ public class utils {
             if (cTable[i] == ch) break;
         }
         return i;
-    }
-
-    public static Date receiverTimeToDate(long delta) {
-        int currentTZOffset = TimeZone.getDefault().getRawOffset();
-        long epochMS = 1230768000000L;  // Jan 01, 2009 00:00 in UTC
-        long milliseconds = epochMS - currentTZOffset;
-        long timeAdd = milliseconds + (1000L * delta);
-        TimeZone tz = TimeZone.getDefault();
-        if (tz.inDaylightTime(new Date())) timeAdd = timeAdd - (1000 * 60 * 60);
-        return new Date(timeAdd);
-    }
-
-    public static GlucoseDataSet[] mergeGlucoseDataRecords(EGVRecord[] egvRecords,
-                                                           SensorRecord[] sensorRecords) {
-        int egvLength = egvRecords.length;
-        int sensorLength = sensorRecords.length;
-        int smallerLength = egvLength < sensorLength ? egvLength : sensorLength;
-        GlucoseDataSet[] glucoseDataSets = new GlucoseDataSet[smallerLength];
-        for (int i = 1; i <= smallerLength; i++) {
-            glucoseDataSets[smallerLength - i] = new GlucoseDataSet(egvRecords[egvLength - i], sensorRecords[sensorLength - i]);
-        }
-        return glucoseDataSets;
     }
 }
